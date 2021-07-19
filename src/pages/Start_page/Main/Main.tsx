a81a18f69fcc4edf9ee02497183b4fb0d7b60594
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2021-07-20 00:37:31 +0300

    sdfsdf
    sdfsdf

---
+++ src/pages/Start_page/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Info from "./Info/Info";
+import Img from "./Img/Img";
+
+function Main() {
+    return (
+        <main className="main">
+            <Img/>
+            <Info/>
+        </main>
+    );
+}
+
+export default Main;
---
+++ src/pages/Start_page/Start_page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+
+function StartPage() {
+    return (
+        <div className="wrapper">
+            <Main/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default StartPage;
